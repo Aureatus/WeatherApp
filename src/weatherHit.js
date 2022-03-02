d91724b4286dd218edd4080878bc325f6c78dfd3
Alex Knott <alextknott@gmail.com>
2022-03-02 14:35:03 +0000

    Added capability to request the data in celsius or fahrenheit

--- src/weatherHit.js
+++ src/weatherHit.js
@@ -1,7 +1,7 @@
-export default async function weatherHit(location) {
+export default async function weatherHit(location, unitState) {
   try {
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=18e25686555e1cb7c85bd078129127c3`,
+      `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=${unitState}&APPID=18e25686555e1cb7c85bd078129127c3`,
       { mode: "cors" }
     );
     const weatherData = await response.json();
