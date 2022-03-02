86619805019d79bc9233285dcd0a90b8935512bc
Alex Knott <alextknott@gmail.com>
2022-03-02 19:12:20 +0000

    Add try and catch to async function

--- src/modules/giphy.js
+++ src/modules/giphy.js
@@ -1,8 +1,12 @@
 export default async function searchGifs(weatherState) {
-  const response = await fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=l9YxwMKg1Scs9CmGIni3C62irA8vpzCF&s=${weatherState} weather&weirdness=2`
-  );
-  const gifData = await response.json();
-  const gifUrl = gifData.data.url;
-  return gifUrl;
+  try {
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=l9YxwMKg1Scs9CmGIni3C62irA8vpzCF&s=${weatherState} weather&weirdness=2`
+    );
+    const gifData = await response.json();
+    const gifUrl = gifData.data.url;
+    return gifUrl;
+  } catch (err) {
+    return err;
+  }
 }
