027b6214f5eaab585d70ff12b89f8ab79a5abd9e
Alex Knott <alextknott@gmail.com>
2022-03-02 19:12:43 +0000

    Created template html file for webpack to use.

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Development",
+      template: "/src/index.html",
     }),
   ],
   output: {
