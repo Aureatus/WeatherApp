eb1d5e7cef1240ee721497fb096270c7472f5d3d
Alex Knott <alextknott@gmail.com>
2022-03-02 21:14:23 +0000

    Added IIFE for initial run with London as the chosen city.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,25 @@ import "normalize.css";
 import "./style.css";
 import requiredWeatherData from "./modules/weather";
 import searchGifs from "./modules/giphy";
+import domFunctions from "./modules/DOM";
 
 let currentLocation = "Mali";
 const currentUnit = "metric";
 
 const form = document.forms[0];
 const searchBar = document.querySelector(".city-search");
+
+(async function initial() {
+  currentLocation = "London";
+  const weatherData = await requiredWeatherData(currentLocation, currentUnit);
+  const gifUrl = await searchGifs(weatherData.weatherState);
+  (await domFunctions()).DOMbuild(weatherData, gifUrl);
+})();
+
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
   currentLocation = searchBar.value;
   const weatherData = await requiredWeatherData(currentLocation, currentUnit);
-  console.log(weatherData);
   const gifUrl = await searchGifs(weatherData.weatherState);
-  console.log(gifUrl);
+  (await domFunctions()).DOMbuild(weatherData, gifUrl);
 });
