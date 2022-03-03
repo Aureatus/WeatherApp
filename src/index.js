49e7312dc376ad4c5187d96900bf62dd1ee2d9fc
Alex Knott <alextknott@gmail.com>
2022-03-03 15:20:56 +0000

    Create functions to change units used via pressing a metric or imperial button.

--- src/index.js
+++ src/index.js
@@ -5,16 +5,34 @@ import searchGifs from "./modules/giphy";
 import domFunctions from "./modules/DOM";
 
 let currentLocation = "Mali";
-const currentUnit = "metric";
+let currentUnit = "metric";
 
 const form = document.forms[0];
 const searchBar = document.querySelector(".city-search");
 
-(async function initial() {
+(async () => {
   currentLocation = "London";
-  const weatherData = await requiredWeatherData(currentLocation, currentUnit);
+  let weatherData = await requiredWeatherData(currentLocation, currentUnit);
   const gifUrl = await searchGifs(weatherData.weatherState);
-  (await domFunctions()).DOMbuild(weatherData, gifUrl);
+  (await domFunctions()).DOMbuild(weatherData, gifUrl, currentUnit);
+  const metricButton = document.querySelector(".metric");
+  const imperialButton = document.querySelector(".imperial");
+
+  metricButton.addEventListener("click", async () => {
+    if (currentUnit === "imperial") {
+      currentUnit = "metric";
+      weatherData = await requiredWeatherData(currentLocation, currentUnit);
+      (await domFunctions()).DOMbuild(weatherData, gifUrl, currentUnit);
+    }
+  });
+
+  imperialButton.addEventListener("click", async () => {
+    if (currentUnit === "metric") {
+      currentUnit = "imperial";
+      weatherData = await requiredWeatherData(currentLocation, currentUnit);
+      (await domFunctions()).DOMbuild(weatherData, gifUrl, currentUnit);
+    }
+  });
 })();
 
 form.addEventListener("submit", async (event) => {
@@ -22,5 +40,5 @@ form.addEventListener("submit", async (event) => {
   currentLocation = searchBar.value;
   const weatherData = await requiredWeatherData(currentLocation, currentUnit);
   const gifUrl = await searchGifs(weatherData.weatherState);
-  (await domFunctions()).DOMbuild(weatherData, gifUrl);
+  (await domFunctions()).DOMbuild(weatherData, gifUrl, currentUnit);
 });
