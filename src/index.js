0d85006e2b6d1982734075ec33fb9c98fe664eca
Alex Knott <alextknott@gmail.com>
2022-03-06 14:16:36 +0000

    Add loading icon whilst waiting for API responses.
    Darkens background as well.

--- src/index.js
+++ src/index.js
@@ -11,11 +11,17 @@ let currentUnit = "metric";
 const form = document.forms[0];
 const searchBar = document.querySelector(".city-search");
 
+document.querySelector(".lds-ring").style.visibility = "hidden";
+
 (async () => {
   currentLocation = "London";
+  document.querySelector(".lds-ring").style.visibility = "visible";
+  document.querySelector(".container").style.opacity = "0.1";
   let weatherData = await requiredWeatherData(currentLocation, currentUnit);
   const gifUrl = await searchGifs(weatherData.weatherState);
   (await domFunctions()).DOMbuild(weatherData, gifUrl, currentUnit);
+  document.querySelector(".lds-ring").style.visibility = "hidden";
+  document.querySelector(".container").style.opacity = "1";
   const metricButton = document.querySelector(".metric");
   const imperialButton = document.querySelector(".imperial");
 
@@ -24,6 +30,8 @@ const searchBar = document.querySelector(".city-search");
       currentUnit = "metric";
       weatherData = await requiredWeatherData(currentLocation, currentUnit);
       (await domFunctions()).DOMbuild(weatherData, gifUrl, currentUnit);
+      metricButton.style.backgroundColor = "palegreen";
+      imperialButton.style.backgroundColor = "#3d3b30";
     }
   });
 
@@ -32,6 +40,8 @@ const searchBar = document.querySelector(".city-search");
       currentUnit = "imperial";
       weatherData = await requiredWeatherData(currentLocation, currentUnit);
       (await domFunctions()).DOMbuild(weatherData, gifUrl, currentUnit);
+      imperialButton.style.backgroundColor = "palegreen";
+      metricButton.style.backgroundColor = "#3d3b30";
     }
   });
 })();
@@ -40,11 +50,17 @@ form.addEventListener("submit", async (event) => {
   event.preventDefault();
   pastLocation = currentLocation;
   currentLocation = searchBar.value;
+  document.querySelector(".lds-ring").style.visibility = "visible";
+  document.querySelector(".container").style.opacity = "0.1";
   const weatherData = await requiredWeatherData(currentLocation, currentUnit);
   if (weatherData instanceof Error) {
     currentLocation = pastLocation;
+    document.querySelector(".lds-ring").style.visibility = "hidden";
+    document.querySelector(".container").style.opacity = "1";
     return;
   }
   const gifUrl = await searchGifs(weatherData.weatherState);
   (await domFunctions()).DOMbuild(weatherData, gifUrl, currentUnit);
+  document.querySelector(".lds-ring").style.visibility = "hidden";
+  document.querySelector(".container").style.opacity = "1";
 });
